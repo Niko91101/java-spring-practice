86539bb6fc9b885fe355c57cab8698753888e16f
Stanislav Muzulev <muz-stanislav@yandex.ru>
2026-04-18 21:06:12 +0300

    initial Spring Boot project setup

---
+++ src/main/java/com/github/niko91101/springpractice/JavaSpringPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.github.niko91101.springpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringPracticeApplication.class, args);
+	}
+
+}
